48bbbd11e05e34682cc215d1792175ab5af5744f
Michalski Grzegorz <C:\Documents and Settings\Administrator\Dane aplikacji\The Bat!>
2014-04-02 15:56:58 +0200

    first commit

---
+++ checkBrowser.js
@@ -0,0 +1,8 @@
+// Check browser - JS script
+//
+// Description:
+// This script check what is a browser client use, and if client are using IE, script show special message on the screen.
+//
+// Author: Michalski Grzegorz
+// Date: 02.04.2014
+
